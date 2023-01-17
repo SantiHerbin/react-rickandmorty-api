1c0944f43f64b4013874888ff2751c904fedfc63
Santiago <50919991+SantiHerbin@users.noreply.github.com>
2023-01-17 12:58:18 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:santiherbin/react-rickandmorty-api.git main:gh-pages
+git push -f git@github.com:santiherbin/rick-morty-api.git main:gh-pages
 
 cd -
