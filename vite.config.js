c9fa54c13413fff4374ec759737faffaa8080978
santiago <santi_herbin@hotmail.com>
2023-01-17 12:54:56 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://santiherbin.github.io/react-rickandmorty-api/",
+  base: "https://santiherbin.github.io/rick-morty-api/",
   plugins: [react()],
 })
